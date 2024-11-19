96b69aff3d3f1ddffe4b8f5c6a2259fd03f8e80d
Vlad <v.zuev@besmart.by>
2024-11-19 18:26:48 +0300

    realize TagService::refreshByExternalId

--- src/main/java/com/besmart/arena/crud/service/TagService.java
+++ src/main/java/com/besmart/arena/crud/service/TagService.java
@@ -1,14 +1,28 @@
 package com.besmart.arena.crud.service;
 
 import com.besmart.arena.crud.dto.Tag;
+import com.besmart.arena.util.JdbcTemplateUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.besmart.arena.util.JdbcTemplateUtil.batchUpdate;
+
 @Service
+@RequiredArgsConstructor
 public final class TagService  {
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public void refreshByExternalId(List<Tag> tags) {
-        throw new UnsupportedOperationException();
+        batchUpdate(
+                jdbcTemplate,
+                tags,
+                """
+                        INSERT INTO tags(external_id, name) VALUES(:externalId, :name)
+                        ON CONFLICT (external_id) DO
+                        UPDATE SET name = :name WHERE tags.external_id = :externalId"""
+        );
     }
 }
