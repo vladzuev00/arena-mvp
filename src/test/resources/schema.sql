23cf1cad315b23fae67c00ae0fca571e771d51f3
Vlad <v.zuev@besmart.by>
2024-11-19 18:43:21 +0300

    add table promoters in schema.sql

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS promoters;
 
 CREATE TABLE categories(
     id SERIAL PRIMARY KEY,
@@ -13,4 +14,12 @@ CREATE TABLE tags(
     id SERIAL PRIMARY KEY,
     external_id INTEGER NOT NULL UNIQUE,
     name VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE promoters(
+    id SERIAL PRIMARY KEY,
+    external_id UUID NOT NULL UNIQUE,
+    name VARCHAR(256) NOT NULL,
+    icon_url VARCHAR(256) NOT NULL,
+    web_page_url VARCHAR(256) NOT NULL
 );
