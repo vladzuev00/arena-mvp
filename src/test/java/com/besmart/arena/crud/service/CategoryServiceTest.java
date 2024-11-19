ce424d58e6e9577058141d6ac0bb417d20f81eb6
Vlad <v.zuev@besmart.by>
2024-11-19 18:20:16 +0300

    refactor CategoryServiceTest

--- src/test/java/com/besmart/arena/crud/service/CategoryServiceTest.java
+++ src/test/java/com/besmart/arena/crud/service/CategoryServiceTest.java
@@ -2,14 +2,12 @@ package com.besmart.arena.crud.service;
 
 import com.besmart.arena.base.AbstractSpringBootTest;
 import com.besmart.arena.crud.dto.Category;
+import com.besmart.arena.crud.rawmapper.CategoryRowMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
 
@@ -20,6 +18,9 @@ public final class CategoryServiceTest extends AbstractSpringBootTest {
 
     @Autowired
     private CategoryService service;
+
+    @Autowired
+    private CategoryRowMapper rowMapper;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -53,26 +54,8 @@ public final class CategoryServiceTest extends AbstractSpringBootTest {
     }
 
     private Set<Category> findAllCategories() {
-        try (var stream = jdbcTemplate.queryForStream("SELECT id, external_id, name, primary_color, secondary_color FROM categories", new CategoryRowMapper())) {
+        try (var stream = jdbcTemplate.queryForStream("SELECT id, external_id, name, primary_color, secondary_color FROM categories", rowMapper)) {
             return stream.collect(toUnmodifiableSet());
-        }
-    }
-
-    private static final class CategoryRowMapper implements RowMapper<Category> {
-        private static final String COLUMN_NAME_ID = "id";
-        private static final String COLUMN_NAME_EXTERNAL_ID = "external_id";
-        private static final String COLUMN_NAME_NAME = "name";
-        private static final String COLUMN_NAME_PRIMARY_COLOR = "primary_color";
-        private static final String COLUMN_NAME_SECONDARY_COLOR = "secondary_color";
-
-        @Override
-        public Category mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
-            long id = resultSet.getLong(COLUMN_NAME_ID);
-            int externalId = resultSet.getInt(COLUMN_NAME_EXTERNAL_ID);
-            String name = resultSet.getString(COLUMN_NAME_NAME);
-            String primaryColor = resultSet.getString(COLUMN_NAME_PRIMARY_COLOR);
-            String secondaryColor = resultSet.getString(COLUMN_NAME_SECONDARY_COLOR);
-            return new Category(id, externalId, name, primaryColor, secondaryColor);
         }
     }
 }
