b4c5a572d674bf9b20a2cba2ff1febd48c8f1505
Vlad <v.zuev@besmart.by>
2024-11-22 10:54:57 +0300

    refactor KakavaClientProperty

--- src/main/java/com/besmart/arena/client/KakavaArenaClient.java
+++ src/main/java/com/besmart/arena/client/KakavaArenaClient.java
@@ -1,33 +1,29 @@
 package com.besmart.arena.client;
 
 import com.besmart.arena.client.domain.ShowsResponseTO;
+import com.besmart.arena.config.property.KakavaClientProperty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.UUID;
-
 import static java.util.Collections.singletonList;
-import static java.util.UUID.fromString;
 import static org.springframework.http.HttpHeaders.ACCEPT_LANGUAGE;
 import static org.springframework.web.util.UriComponentsBuilder.fromHttpUrl;
 
-//TODO: refactor and tests
 @Component
 @RequiredArgsConstructor
 public final class KakavaArenaClient implements ArenaClient {
     private static final String VENUES_PARAM_NAME = "venues";
     private static final String GETTING_SHOWS_URL = "https://app-kkv-be-test.azurewebsites.net/api/v1/event/show";
-    private static final String LANGUAGE = "EN";
-    private static final UUID VENUE_ID = fromString("a60de864-5c52-11ee-a81c-000d3aa868a2");
 
     private final WebClient webClient;
+    private final KakavaClientProperty property;
 
     @Override
     public ShowsResponseTO request() {
         return webClient.get()
                 .uri(buildGettingShowsUri())
-                .header(ACCEPT_LANGUAGE, LANGUAGE)
+                .header(ACCEPT_LANGUAGE, property.getLanguage())
                 .retrieve()
                 .bodyToMono(ShowsResponseTO.class)
                 .block();
@@ -35,7 +31,7 @@ public final class KakavaArenaClient implements ArenaClient {
 
     private String buildGettingShowsUri() {
         return fromHttpUrl(GETTING_SHOWS_URL)
-                .queryParam(VENUES_PARAM_NAME, singletonList(VENUE_ID))
+                .queryParam(VENUES_PARAM_NAME, singletonList(property.getVenueId()))
                 .toUriString();
     }
 }
