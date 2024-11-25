c0329b9fe61b3ec3ffb9cac5c02b57794923d2a8
Vlad <v.zuev@besmart.by>
2024-11-25 15:05:19 +0300

    refactor EventRowMapper and EventRowMapperTest

--- src/main/java/com/besmart/arena/crud/rowmapper/EventRowMapper.java
+++ src/main/java/com/besmart/arena/crud/rowmapper/EventRowMapper.java
@@ -28,14 +28,13 @@ public final class EventRowMapper implements RowMapper<Event> {
     public Event mapRow(ResultSet resultSet, int rowNumber)
             throws SQLException {
         Long id = resultSet.getLong(ALIAS_ID);
-        int externalShortId = resultSet.getInt(ALIAS_EXTERNAL_SHORT_ID);
+        String externalShortId = resultSet.getString(ALIAS_EXTERNAL_SHORT_ID);
         String title = resultSet.getString(ALIAS_TITLE);
         String subtitle = resultSet.getString(ALIAS_SUBTITLE);
         String description = resultSet.getString(ALIAS_DESCRIPTION);
         LocalDateTime dateTime = resultSet.getTimestamp(ALIAS_DATE_TIME).toLocalDateTime();
         Show show = getShowLazily(resultSet, ALIAS_SHOW_ID);
         Provider provider = getProviderLazily(resultSet, ALIAS_PROVIDER_ID);
-        return null;
-//        return new Event(id, externalShortId, title, subtitle, description, dateTime, show, provider);
+        return new Event(id, externalShortId, title, subtitle, description, dateTime, show, provider);
     }
 }
